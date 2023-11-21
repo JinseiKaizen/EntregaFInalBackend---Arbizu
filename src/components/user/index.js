ab7ba475111448cf1b1fe0080d5e4692f639d540
ALDO ALBERTO ARBIZU <69614658+JinseiKaizen@users.noreply.github.com>
2023-11-20 23:27:37 -0300

    0.1

---
+++ src/components/user/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const userController = require("./controller/userController");
+module.exports = (app) =>{
+    const router = new Router();
+    app.use('/user', router);
+    router.get('/:user_id', userController.get);
+    router.get('/', userController.getAll);
+    router.post('/', userController.create);
+
+    router.get('/inactive', userController.getAllInactive);
+    router.delete('/inactive', userController.deleteInactiveUsers);
+}
